295856159393603bc7d915d41310b1a973560cd1
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2023-12-16 11:19:11 +0100

    tie

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
 from flask import Flask, flash, request,send_from_directory,current_app ,redirect, url_for
 import os
 from werkzeug.utils import secure_filename
-
+from confluent_kafka import Producer
+from confluent_kafka import Consumer
+import logging
+import json
+import subprocess
+PARTITION_GRANULARITY=1024
 app=Flask(__name__)
 UPLOAD_FOLDER = 'downloadable'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
+FILESYSTEM_DIMENSION=100#Mb
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+p=Producer({'bootstrap.servers':'localhost:9092'})
+c=Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
+
+print('Topics disponibili da consumare: ', c.list_topics().topics)
+c.subscribe(['FirstCallAck'])
+
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='producer.log',
+                    filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+# Logging e Stampa dei messaggi prodotti
+def receipt(err,msg):
+    if err is not None:
+        print('Error: {}'.format(err))
+    else:
+        message = 'Prodotto un messaggio sul topic {} con il valore {}\n'.format(msg.topic(), msg.value().decode('utf-8'))
+        logger.info(message)
+        print(message)
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/", methods=["GET"])
 def init():
 
@@ -26,13 +59,13 @@ def init():
         output += "</a>"
     output+="</body> </html>"
     return output
-app.register_blueprint
-@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+
+
+
 def download(filename):
     uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
     return send_from_directory(uploads, filename)
 
-@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -58,3 +91,33 @@ def upload_file():
         '''
 
 app.run(debug=True,port=80)
+def entry_topic():
+    bashCommandName = "echo $NAME"
+
+    ContainerName = subprocess.check_output(['bash','-c', bashCommandName]) 
+
+    data={"Name":ContainerName,
+          "Dim":FILESYSTEM_DIMENSION}
+    m=json.dumps(data)
+    p.poll(1)
+    p.produce('FirstCall', m.encode('utf-8'),callback=receipt)
+    p.flush()
+    while True:
+            msg=c.poll(1.0) #timeout
+            if msg is None:
+                continue
+            elif msg.error():
+                print('Error: {}'.format(msg.error()))
+                continue
+            else:
+                data=msg.value().decode('utf-8')
+                print(data)
+                break
+    c.close()
+    c.unsubscribe()
+    download=Consumer.subscribe([data["downloadTopic"]])
+    
+
+if __name__== "main":
+    entry_topic()
+   
