7aeb615ef85037c6829b2bb2872a7cb3cb5c2dc5
Carmelo Angelo Federico Ragusa <federicoragusa@hotmail.com>
2023-12-19 12:34:39 +0100

    File di inizializzazione database (.txt) e aggiunta codice per accedere al database nel manager.py

--- manager.py
+++ manager.py
@@ -1,5 +1,23 @@
 from confluent_kafka import AdminClient, NewTopic
+import mysql.connector
 
 admin = AdminClient({'bootstrap.servers': 'localhost:9092'})
 
 admin.create_topics([NewTopic("FirstCall", num_partitions=1, replication_factor=1), NewTopic("FirstCallAck", num_partitions=1, replication_factor=1)])
+
+db = mysql.connector.connect(
+    host = "localhost",
+    database = "ds_filesystem",
+    user = "admin",
+    password = "admin",
+    port = 3307
+)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT MAX(topic) FROM partitions")
+
+res = cursor.fetchall()
+
+for x in res:
+  print(x)
