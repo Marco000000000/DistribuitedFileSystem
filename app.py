ee7ab33d934e3cbf3fa8c07626957516fc503f0a
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2023-12-22 23:47:16 +0100

    ho fatto alcuni fix

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from flask import Flask, flash, request,send_from_directory,current_app ,redirect, url_for
 import os
 from werkzeug.utils import secure_filename
 from confluent_kafka import Producer
 from confluent_kafka import Consumer
 import logging
 import json
-import subprocess
 import base64
 import random
 import string
 
 PARTITION_GRANULARITY=1024
-app=Flask(__name__)
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", default = 'downloadable')
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 FILESYSTEM_DIMENSION=os.getenv("FILESYSTEM_DIMENSION", default = 100)#Mb
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#creazione di una stringa random 
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
-    print("Random string of length", length, "is:", result_str)
 
+#prima coppia di producer-consumer
 p=Producer({'bootstrap.servers':'localhost:9092'})
 c=Consumer({'bootstrap.servers':'localhost:9092','group.id':get_random_string(20),'auto.offset.reset':'earliest'})
 
@@ -37,7 +35,7 @@ logger.setLevel(logging.INFO)
 
 print('Topics disponibili da consumare: ', c.list_topics().topics)
 c.subscribe(['FirstCallAck'])
-# Logging e Stampa dei messaggi prodotti
+# Logging e Stampa dei messaggi prodotti (Callback)
 def receipt(err,msg):
     if err is not None:
         print('Error: {}'.format(err))
@@ -46,65 +44,48 @@ def receipt(err,msg):
         logger.info(message)
         print(message)
 
-
+#check per le estensioni permesse
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=["GET"])
-def init():
-
-    dir1=os.listdir( app.config['UPLOAD_FOLDER'])#dir1=uos.ilistdir("/sd")
-    output=" "
-    output="<html> <head></head><body> "
-    for file in dir1:
-        output+=" "
-        output += "<a href=\"download/"
-        output += file#str(file[0])
-        output += "\">"
-        output += "<br>"
-        output +=file[0:]#str(file)
-        output += "</a>"
-    output+="</body> </html>"
-    return output
 
 
-#mettere funzione che da un solo pacchetto
+#Invio di un file spezzato con la granularità predefinita
 def download_file(filename,topicNumber):
     topicName="Download"+topicNumber
     if filename is not None and  allowed_file(filename):
-        directory = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'],filename)
+        directory = os.path.join( UPLOAD_FOLDER,filename)
         index=0
         with open(directory, "rb") as f:
             while (byte := f.read(PARTITION_GRANULARITY)):
                 data={
                     
-                    "data":base64.b64encode(byte)
+                    "data":str(base64.b64encode(byte),"UTF-8"),
                     }
                 m=json.dumps(data)
                 p.poll(1)
                 p.produce(topicName, m.encode('utf-8'),callback=receipt)
                 p.flush()
                 return 
-        
+
+#upload di un singolo pacchetto di dati 
 def upload_file(filename,pack):
-    directory = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'],filename)
+    directory = os.path.join( UPLOAD_FOLDER,filename)
 
     file = base64.b64decode(pack["data"])
 
     with open(directory, "ab+") as f:
         f.write(file)
     
-
-app.run(debug=True,port=80)
+#Chiamata per la registrazione nei topic kafka
 def first_Call():
-    data={"Name":get_random_string(20),
+    data={"Code":get_random_string(20),
           "Dim":FILESYSTEM_DIMENSION}
     m=json.dumps(data)
     p.poll(1)
     p.produce('FirstCall', m.encode('utf-8'),callback=receipt)
-    p.flush()
     while True:
             msg=c.poll(1.0) #timeout
             if msg is None:
@@ -113,20 +94,20 @@ def first_Call():
                 print('Error: {}'.format(msg.error()))
                 continue
             else:
-                data=msg.value().decode('utf-8')
+                data=json.loads(msg.value().decode('utf-8'))
                 print(data)
                 break
     c.close()
     c.unsubscribe()
 
     return data["id"],data["topic"]
-
+#eliminazione file 
 def delete_file(filename):
     if os.path.exists(filename):
         os.remove(filename)
 
 if __name__== "main":
-    id,topicNumber=first_Call()
+    id,topicNumber=first_Call() #ricezione dati necessari per la ricezione
     uploadConsumer=Consumer({'bootstrap.servers':'localhost:9092','group.id':str(id),'auto.offset.reset':'earliest'})
     uploadConsumer.subscribe("Upload"+topicNumber)
     requestConsumer=Consumer({'bootstrap.servers':'localhost:9092','group.id':"000",'auto.offset.reset':'earliest'})
@@ -144,9 +125,9 @@ if __name__== "main":
             print('Error: {}'.format(msg.error()))
             continue
         else:
-            data=msg.value().decode('utf-8')
+            data=json.loads(msg.value().decode('utf-8'))
             if data["fileName"]!="":
-                download_file(data["fileName"],topicNumber)
+                download_file(secure_filename(data["fileName"]),topicNumber)
 
         if msgUpload is None:
             continue
@@ -154,9 +135,9 @@ if __name__== "main":
             print('Error: {}'.format(msg.error()))
             continue
         else:
-            data=msgUpload.value().decode('utf-8')
+            data=json.loads(msgUpload.value().decode('utf-8'))
             if data["fileName"]!="":
-                upload_file(data["fileName"],data)
+                upload_file(secure_filename(data["fileName"]),data)
                 
         if msgDelete is None:
             continue
@@ -164,8 +145,6 @@ if __name__== "main":
             print('Error: {}'.format(msg.error()))
             continue
         else:
-            data=msgDelete.value().decode('utf-8')
+            data=json.loads(msgDelete.value().decode('utf-8'))
             if data["fileName"]!="":
-                delete_file(data["fileName"])                
-                
-            
+                delete_file(secure_filename(data["fileName"]))   
