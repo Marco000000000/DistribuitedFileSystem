6b0bf2403e0d45b5b2e5e1f530dc9e3e04a2370a
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2023-12-30 16:13:23 +0100

    nginx forse fatto

--- request.lua
+++ request.lua
@@ -1,49 +1,99 @@
 -- lua-scripts/main.lua
+
+
+local mysql = require("resty.mysql")
+local cjson = require("cjson")
+
+local mysql_query = "select controller_name from controller join controllertopic on controller.id_controller=controllertopic.id_controller where topic in (select distinct topic from partitions join file on partition_id=id where file_name=%s) order by topic"
+
+-- MySQL connection settings
+local db, err = mysql:new()
+if not db then
+    ngx.status = 500
+    ngx.say("Failed to initialize MySQL: ", err)
+    return
+end
+
+db:set_timeout(1000) -- 1 sec
+
+local ok, err, errno, sqlstate = db:connect{
+    host = "localhost",
+    port = 3307,
+    database = "ds_filesystem",
+    user = "root",
+    password = "giovanni",
+}
+
+if not ok then
+    ngx.status = 500
+    ngx.say("Failed to connect to MySQL: ", err)
+    return
+end
+
+local res, err, errno, sqlstate = db:query(mysql_query)
+if not res then
+    ngx.status = 500
+    ngx.say("Failed to query MySQL: ", err)
+    return
+end
+
+local len=#res
+local toCapture={}
 local original_request_uri_args = ngx.req.get_uri_args()
 
--- Construct the subrequest URI with the same parameters
-local subrequest_uri = "/backend" .. ngx.var.uri
-local subrequest_args = ngx.encode_args(original_request_uri_args)
-if subrequest_args ~= "" then
-    subrequest_uri = subrequest_uri .. "?" .. subrequest_args
+for i, raw in pairs(res) do
+
+    local subrequest_uri = raw["controller_name"] .. ngx.var.uri
+    local subrequest_args = ngx.encode_args(original_request_uri_args)
+    if subrequest_args ~= "" then
+        subrequest_uri = subrequest_uri .. "?" .. subrequest_args
+    end
+    
+    toCapture[i]=subrequest_uri
 end
 
--- Make the subrequest
-local response, err = ngx.location.capture(subrequest_uri)
-local response1, err1 = ngx.location.capture("/dManager1")
-local response2, err2 = ngx.location.capture("/dManager2")
+db:close()
 
-if err1 or err2 then
-    ngx.status = 500
-    ngx.say("Internal Server Error")
-    return ngx.exit(ngx.status)
-end
+local response={}
 
--- Concatenate chunks of responses into a single body
-local concatenated_body = ""
---while i <= length do
---    local start_index = i
---    local end_index = i + chunk_size - 1
---    local chunk = string.sub(body, start_index, end_index)
---    table.insert(chunks, chunk)
---
---    -- Check if this is the last chunk
---    if end_index >= length then
---        break
---    end
---
---    i = end_index + 1
---end
-for _, res in ipairs(responses) do
-    if res.body then
-        -- Read chunks of the specified size
-        for i = 1, #res.body, tonumber(ngx.var.chunk_size) do
-            concatenated_body = concatenated_body .. string.sub(res.body, i, i + tonumber(ngx.var.chunk_size) - 1)
-        end
-    end-- Method 1: Using string.len
+for i,raw in pairs(toCapture) do
+
+    local response[i], err = ngx.location.capture(subrequest_uri)
+    if err then
+        ngx.status = 500
+        ngx.say("Internal Server Error")
+        return ngx.exit(ngx.status)
+    end
 
 end
 
 ngx.status = 200
-ngx.say(concatenated_body)
+local e=0
+-- Concatenate chunks of responses into a single body
+local condiction=true
+while condiction then
+
+    local concatenated_body = ""
+    for i,res in pairs(response) do
+
+        local start_index = e
+        local end_index = e + chunk_size - 1
+        local tempString=string.sub(res.body, i, i + tonumber(ngx.var.chunk_size) - 1)
+        concatenated_body = concatenated_body .. tempString
+        if string.len(tempString)<chunk_size then
+            condiction=false
+
+            break
+        end
+    -- Check if this is the last chunk
+        if end_index >= length then
+            break
+        end
+
+        e = end_index + 1
+    end
+    ngx.say(concatenated_body)
+end
 ngx.eof()
+
+
