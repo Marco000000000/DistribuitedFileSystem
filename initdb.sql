7314d1e88496dec06e1c04db30d3266720870ea4
FrendtyLaptop <federicoragusa@hotmail.com>
2023-12-31 16:09:34 +0100

    Creazione utente file_manager

--- initdb.sql
+++ initdb.sql
@@ -33,3 +33,14 @@ CREATE TABLE IF NOT EXISTS controllertopic (
     PRIMARY KEY (id_controller,topic),
     FOREIGN KEY (id_controller) REFERENCES controller (id_controller)
 );
+
+
+-- Creazione utente file_manager
+CREATE USER 'file_manager'@'localhost' IDENTIFIED BY 'file';
+
+GRANT SELECT ON ds_filesystem.file TO file_manager@localhost; -- Permesso di lettura
+GRANT INSERT ON ds_filesystem.file TO file_manager@localhost; -- Permesso di inserimento di record all'interno della tabella
+GRANT UPDATE ON ds_filesystem.file TO file_manager@localhost; -- Permesso di modifica dei record presenti nella tabella
+GRANT DELETE ON ds_filesystem.file TO file_manager@localhost; -- Permesso di eliminazione dei record dalla tabella
+
+FLUSH PRIVILEGES;
