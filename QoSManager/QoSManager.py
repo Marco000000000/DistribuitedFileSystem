0cbd27a78807d050aab6f2607a8e85ff8bca3439
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2024-01-22 18:32:48 +0100

    .

--- QoSManager/QoSManager.py
+++ QoSManager/QoSManager.py
@@ -17,11 +17,41 @@ def createDownloadManager():
 
 
 def createFileSystem():
-    deploymentName="filesystem-deployment"
-    namespace="dafault"
-    update_replicas(deploymentName,-1,namespace)
+    # deploymentName="filesystem-deployment"
+    # namespace="dafault"
+    # update_replicas(deploymentName,-1,namespace)
+    namespace = "default"
+    deployment_name = "filesystem-deployment"
+    image = "distribuitedfilesystem-filesystem1:latest"
+    replicas = 2
+    create_deployment(namespace, deployment_name, image, replicas)
     return
+def create_deployment(namespace, deployment_name, image, replicas):
+    # Load the Kubernetes configuration
+    config.load_kube_config()
 
+    # Create a Kubernetes API client
+    apps_api = client.AppsV1Api()
+
+    # Define the deployment spec
+    deployment = client.V1Deployment(
+        metadata=client.V1ObjectMeta(name=deployment_name),
+        spec=client.V1DeploymentSpec(
+            replicas=replicas,
+            selector=client.V1LabelSelector(
+                match_labels={"app": deployment_name}
+            ),
+            template=client.V1PodTemplateSpec(
+                metadata=client.V1ObjectMeta(labels={"app": deployment_name}),
+                spec=client.V1PodSpec(
+                    containers=[client.V1Container(name=deployment_name, image=image, image_pull_policy="Never")]
+                ),
+            ),
+        ),
+    )
+
+    # Create the deployment
+    apps_api.create_namespaced_deployment(namespace, deployment)
 def update_replicas(deployment_name, new_replica_count, namespace):
     config.load_incluster_config()  # Load in-cluster config
 
@@ -78,6 +108,6 @@ def update_prometheus_config():
 
 if __name__ == "__main__":
     time.sleep(10)
+    createFileSystem()
     createUploadManager()
     createDownloadManager()
-    createFileSystem()
