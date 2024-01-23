940b7b747f5afde99a53b86c4b8c9dc359ada9e5
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2024-01-23 12:14:49 +0100

    prova aggiunta replica

--- QoSManager/QoSManager.py
+++ QoSManager/QoSManager.py
@@ -31,9 +31,53 @@ def create_deployment(api_instance, deployment_name, container_image, replicas):
         body=deployment_manifest,
         namespace="default"
     )
+def createUploadManager():
+    deploymentName="uploadcontroller-deployment"
+    namespace="dafault"
+    update_replicas(deploymentName,-1,namespace)
+    return
+
+def createDownloadManager():
+    deploymentName="download-controller-deployment"
+    namespace="dafault"
+    update_replicas(deploymentName,-1,namespace)
+    return
+
+
+def createFileSystem():
+    deploymentName="filesystem-deployment"
+    namespace="dafault"
+    update_replicas(deploymentName,-1,namespace)
+    return
+
+def update_replicas(deployment_name, new_replica_count, namespace):
+    config.load_incluster_config()  # Load in-cluster config
+
+    api_instance = client.AppsV1Api()
+
+    # Get the deployment
+    deployment = api_instance.read_namespaced_deployment(
+        name=deployment_name,
+        namespace=namespace
+    )
+
+    # Update the replicas field
+    if new_replica_count==-1:
+        deployment.spec.replicas=deployment.spec.replicas+1
+    else:
+        deployment.spec.replicas = new_replica_count
+
+    # Apply the changes
+    api_instance.replace_namespaced_deployment(
+        name=deployment_name,
+        namespace=namespace,
+        body=deployment
+    )
+    print(f"Replicas for '{deployment_name}' set to {new_replica_count}.") 
 
 def main():
     # Load in-cluster Kubernetes configuration
+    createFileSystem()
     config.load_incluster_config()
 
     # Create the Kubernetes API client
