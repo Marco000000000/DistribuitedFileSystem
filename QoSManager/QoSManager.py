97e1c64cde568a4321cf540115bd2430cc922b24
Frendty <federicoragusa@hotmail.com>
2024-01-26 23:04:41 +0100

    WIP queries API

--- QoSManager/QoSManager.py
+++ QoSManager/QoSManager.py
@@ -8,6 +8,9 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Dizionario per tenere traccia delle regole di QoS
+sla_rules = {}
+
 prometheus_url = "http://prometheus-service:9090"
 prometheus = PrometheusConnect(url=prometheus_url, disable_ssl=True)
 
@@ -91,7 +94,15 @@ def createUploadManager():
     print("creato un "+name)
     #logger.info("creato un "+name)
 
-# Sezione recupero metriche da prometheus
+# Sezione gestione SLA e Prometheus
+@app.route('/update_sla_rule', methods=['POST'])
+def update_sla_rule(sla_rule, sla_value = 0):
+    # Ricevere questi dati non dai richieste http con gli altri nodi del sistema, ma con una lettura al database
+    # Inoltre definire una range di valori ammissibili per la metrica ricevuta
+    sla_rules[sla_rule]['current_value'] = sla_value
+    #sla_rules[sla_rule]['threshold'] = 0 #Aggiungere campo threshold all'api update_sla_rule (dovrebbe essere lui il valore desidereto?)
+    return jsonify({"message": "SLA rule updated successfully"}), 200
+        
 
 # Queries possibili per il download
 # download_file_latency_query = 'download_file_latency_seconds'
@@ -100,61 +111,40 @@ def createUploadManager():
 @app.route('/query', methods=['GET'])
 def query_prometheus():
     query = request.args.get('query', '')
-    range = request.args.get('range', 'false').lower() == 'true'
+    ranged_query = request.args.get('range', 'false').lower() == 'true'
     start_time = request.args.get('start', '1')
     start_time ="now-"+start_time+"h"
     end_time = request.args.get('end', 'now')
     step = request.args.get('step', '15s')
-    aggregation = request.args.get('aggregation', '')
+    aggregation = request.args.get('aggregation', '').lower()
     rate_interval = request.args.get('rate_interval', '5m')
     
     allowed_queries = ['download_file_latency_seconds', 'download_file_throughput_bytes']
+    allowed_aggregations = ['avg', 'min', 'max', 'sum', 'count', 'stddev', 'stdvar', 'rate', '']
     
     try:
-        if range:
+        if ranged_query:
             if query in allowed_queries:
-                if aggregation == '':
-                    result = prometheus.custom_query_range(query, start=start_time, end=end_time, step=step)
-                elif aggregation == 'avg':
-                    result = prometheus.custom_query_range(f'avg({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'min':
-                    result = prometheus.custom_query_range(f'min({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'max':
-                    result = prometheus.custom_query_range(f'max({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'sum':
-                    result = prometheus.custom_query_range(f'sum({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'count':
-                    result = prometheus.custom_query_range(f'count({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'stddev':
-                    result = prometheus.custom_query_range(f'stddev({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'stdvar':
-                    result = prometheus.custom_query_range(f'stdvar({query})', start=start_time, end=end_time, step=step)
-                elif aggregation == 'rate':
-                    result = prometheus.custom_query_range(f'rate({query}[{rate_interval}])', start=start_time, end=end_time, step=step)
+                if aggregation in allowed_aggregations:
+                    if aggregation == '':
+                        result = prometheus.custom_query_range(query, start=start_time, end=end_time, step=step)
+                    elif aggregation != 'rate':
+                        result = prometheus.custom_query_range(f'{aggregation}({query})', start=start_time, end=end_time, step=step)
+                    else:
+                        result = prometheus.custom_query_range(f'{aggregation}({query}[{rate_interval}])', start=start_time, end=end_time, step=step)
                 else:
                     return jsonify({"error": "Invalid aggregation"}), 400
             else:
                 return jsonify({"error": "Invalid query"}), 400
         else:
             if query in allowed_queries:
-                if aggregation == '':
-                    result = prometheus.custom_query(query)
-                elif aggregation == 'avg':
-                    result = prometheus.custom_query(f'avg({query})')
-                elif aggregation == 'min':
-                    result = prometheus.custom_query(f'min({query})')
-                elif aggregation == 'max':
-                    result = prometheus.custom_query(f'max({query})')
-                elif aggregation == 'sum':
-                    result = prometheus.custom_query(f'sum({query})')
-                elif aggregation == 'count':
-                    result = prometheus.custom_query(f'count({query})')
-                elif aggregation == 'stddev':
-                    result = prometheus.custom_query(f'stddev({query})')
-                elif aggregation == 'stdvar':
-                    result = prometheus.custom_query(f'stdvar({query})')
-                elif aggregation == 'rate':
-                    result = prometheus.custom_query(f'rate({query}[{rate_interval}])')
+                if aggregation in allowed_aggregations:
+                    if aggregation == '':
+                        result = prometheus.custom_query(query)
+                    elif aggregation != 'rate':
+                        result = prometheus.custom_query(f'{aggregation}({query})')
+                    else:
+                        result = prometheus.custom_query(f'{aggregation}({query}[{rate_interval}])')
                 else:
                     return jsonify({"error": "Invalid aggregation"}), 400
             else:
