0e69e2b64702e6cadc8cc2c55bf904d11101053a
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2024-01-27 21:40:19 +0100

    .

--- QoSManager/client.py
+++ QoSManager/client.py
@@ -1,26 +1,35 @@
+import time
 import requests
 import random
 import threading
 
 def makeRandomRequest():
         response=requests.get("http://192.168.1.128:30000/discover")
-
+        print(response)
         names=response.json()
-        lenght=len(names)
         random_key = random.choice(list(names.keys()))
+        url="http://192.168.1.128:30000/download/"+names[random_key]
+        print(url)
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                print(chunk)
+        
 
-        print("http://192.168.1.128:30000/download/"+random_key)
-        response=requests.get("http://192.168.1.128:30000/download/"+random_key)
-        if response.status_code == 200:
-            for chunk in response.iter_content(chunk_size=131072):  # Adjust the chunk size as needed
-                pass
+        print("fatto")
 if __name__=="__main__":
+    #makeRandomRequest()
     while True:
         threads=[]
-        for i in range(10):
+        for i in range(3):
             thread = threading.Thread(target=makeRandomRequest)
             threads.append(thread)
             thread.start()
+            time.sleep(2)
 
-        for i in range(10):
-            threads[i].join()
+        
+        
+        time.sleep(4)
